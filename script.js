427e117eccbc4443e81725e6e025b50bfb420e48
jianayra30-alt <jianayra30@gmail.com>
2025-10-28 18:43:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementByld('sapaButton');
+const tombolSapa = document.getElementById('sapaButton');
+
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
-   // 3. Jalankan kode ini ketika tombol di-klik
-   alert('Halo! Terima kasih sudah berkuncung!');
-}); 
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Halo! Terima kasih sudah berkunjung!');
+});
